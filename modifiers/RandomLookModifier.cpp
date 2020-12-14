17ae8299caf8b8ff333ac5a713292fcbda24ae7f
contact@danielkorgel.com <264778848eca87c3d106916245f7c66c>
2020-12-14 21:24:10 +0100

    RandomLookModifier: Limited random y range to -0.6 to 0.6

--- modifiers/RandomLookModifier.cpp
+++ modifiers/RandomLookModifier.cpp
@@ -18,7 +18,7 @@ void EyeBehavior::RandomLookModifier::update(unsigned long now, EyeConfig* curre
         time_last_random_pos = now;
         if(getModifier() > 0) { // skip actions if they would not be visible anyway
             float rndpos_x = randomFloat(0, 2) - 1;
-            float rndpos_y = randomFloat(0, 2) - 1;
+            float rndpos_y = randomFloat(0.4, 1.6) - 1;
             float rndPupilSize = randomFloat(current_eye_config->pupil_size);
             eyeTargetValues->eye_pos.x = lerp(eyeTargetValues->eye_pos.x, rndpos_x, getModifier());
             eyeTargetValues->eye_pos.y = lerp(eyeTargetValues->eye_pos.y, rndpos_y, getModifier());
